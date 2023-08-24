137c380f58b8c1cf16aeac08eb46672f9cadd072
Raj <rajdristant007@gmail.com>
2023-08-24 19:46:03 +0530

    Fixed bug of sync with local database

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ToDo {
   String? id;
   String? todoText;
@@ -9,13 +11,46 @@ class ToDo {
     this.isDone = false,
   });
 
+  @override
+  String toString() {
+    return json.encode({
+      "id": id,
+      "todoText": todoText,
+      'isDone': isDone,
+    });
+  }
+
+  static ToDo fromString(String dataString) {
+    dynamic data = json.decode(dataString);
+    return ToDo(
+      id: data['id'],
+      todoText: data['todoText'],
+      isDone: data['isDone'],
+    );
+  }
+
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Write Practicals', ),
-      ToDo(id: '02', todoText: 'Buy Supplies',  ),
-      ToDo(id: '03', todoText: 'Check Emails', ),
-      ToDo(id: '04', todoText: 'Finish Assignment', ),
-      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
+      ToDo(
+        id: '01',
+        todoText: 'Write Practicals',
+      ),
+      ToDo(
+        id: '02',
+        todoText: 'Buy Supplies',
+      ),
+      ToDo(
+        id: '03',
+        todoText: 'Check Emails',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'Finish Assignment',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Work on mobile apps for 2 hour',
+      ),
     ];
   }
 }
